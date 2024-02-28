8d69b8fa61220383dff850b906d4e410aa1637a1
goodgans <mityai.tiunov@gmail.com>
2024-02-28 15:47:32 +0300

    task5

---
+++ lesson1/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// вычислить значение формулы, где a,b,c,d - некоторые целые числа. 
+double CalculateFormula (int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
