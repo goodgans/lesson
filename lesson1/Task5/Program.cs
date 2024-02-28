021c7156ca0e6ae98a39a03eb34811ade8d07c44
goodgans <mityai.tiunov@gmail.com>
2024-02-28 17:19:48 +0300

    task5

--- lesson1/Task5/Program.cs
+++ lesson1/Task5/Program.cs
@@ -1,11 +1,46 @@
 ﻿// вычислить значение формулы, где a,b,c,d - некоторые целые числа. 
-double CalculateFormula (int a, int b, int c, int d)
+//double CalculateFormula (int a, int b, int c, int d)
+//{
+//    double numenator = a * b;
+//    int denumenator = c + d;
+//    double result = numenator / denumenator;
+//    return result;
+//}
+
+//double result = CalculateFormula(1, 2, 3, 4);
+//Console.WriteLine(result);
+
+//void PrintSquares (int limit)
+//{
+//    int i = 1;
+//    while (i <=limit)
+//    {
+//        Console.Write($"{i*i} ");
+//        i++;
+//    }
+//}
+//PrintSquares(5);
+//PrintSquares(10);
+//PrintSquares(15);
+
+void ZeroEvenElements(int[] arr)
 {
-    double numenator = a * b;
-    int denumenator = c + d;
-    double result = numenator / denumenator;
-    return result;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
 }
 
-double result = CalculateFormula(1, 2, 3, 4);
-Console.WriteLine(result);
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArray(array);
--- lesson1/Task5/obj/Debug/net8.0/Task5.AssemblyInfo.cs
+++ lesson1/Task5/obj/Debug/net8.0/Task5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7494288e8d69bcc058668889dbaf23a0629a8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d69b8fa61220383dff850b906d4e410aa1637a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
